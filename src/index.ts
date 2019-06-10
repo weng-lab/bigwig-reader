df05604f1d460474fcf65756ce54d9096e1992f9
hpratt <hpratt111@live.com>
2019-06-09 21:54:53 -0400

    add two bit reader export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 export { HeaderData, FileType, CommonHeader, ZoomLevelHeader, BWTotalSummary, ChromTree } from "./BigWigHeaderReader";
 export { BigWigData, BigBedData, BigZoomData, BigWigReader } from "./BigWigReader";
 export { DataLoader, AxiosDataLoader } from "./DataLoader";
+
+import TwoBitReader from './TwoBitReader';
+export { TwoBitReader };
