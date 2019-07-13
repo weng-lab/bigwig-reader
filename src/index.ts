cdbabd396b9ff9daec220ea7738b17168d406d02
hpratt <hpratt111@live.com>
2019-07-12 21:42:48 -0400

    export error type enum

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export { HeaderData, FileType, CommonHeader, ZoomLevelHeader, BWTotalSummary, ChromTree } from "./BigWigHeaderReader";
 export { BigWigData, BigBedData, BigZoomData, BigWigReader } from "./BigWigReader";
-export { DataLoader, AxiosDataLoader, FileDataLoader } from "./DataLoader";
+export { DataLoader, AxiosDataLoader, FileDataLoader, ErrorType } from "./DataLoader";
