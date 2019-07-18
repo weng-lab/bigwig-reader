3b5d108190e502709f398708adcbf9a4880ac33d
hpratt <hpratt111@live.com>
2019-07-18 07:45:21 -0400

    fix error where stream was sometimes not ready when read was called

--- src/GoogleDataLoader.ts
+++ src/GoogleDataLoader.ts
@@ -1,11 +1,6 @@
 import { Storage, Bucket, File } from '@google-cloud/storage';
 import { DataLoader } from './DataLoader';
 
-interface Result {
-    done: boolean;
-    value?: Uint8Array;
-};
-
 /**
  * DataLoader for reading data from a file in a GCP storage bucket.
  */
@@ -22,26 +17,31 @@ export class GoogleBucketDataLoader implements DataLoader {
     }
 
     async load(start: number, size?: number): Promise<ArrayBuffer> {
-	const bsize = size || (await this.file_.getMetadata({
+	
+	const bsize = size || +(await this.file_.getMetadata({
 	    userProject: this.billedProject
 	}))[0].size - start;
 	const stream = await this.file_.createReadStream({
 	    userProject: this.billedProject,
 	    start,
-	    end: size ? start + size : start + bsize
+	    end: start - 1 + (size || bsize)
 	});
+	if (!stream) throw new Error("could not open gs://" + this.bucket + "/" + this.file);
+	
 	const buffer: Uint8Array = new Uint8Array(bsize);
 	let ptr: number = 0;
-	let result: Result = { done: false };
-	do {
-	    const result: Result = await stream.read();
-	    if (result.value !== undefined) {
-		buffer.set(result.value, ptr);
-		ptr += result.value.length;
+	for await (const chunk of stream) {
+	    if (chunk !== undefined && chunk !== null && chunk.length) {
+		buffer.set(chunk, ptr);
+		ptr += chunk.length;
 	    } else
-		throw new Error("could not read " + size + " bytes from gs://" + this.bucket + "/" + this.file);
-	} while (!result.done);
+		throw new Error("could not read " + size + " bytes from gs://" + this.bucket + "/" + this.file + "; did read " + ptr);
+	}
+	if (ptr !== buffer.length)
+	    throw new Error("could not read " + size + " bytes from gs://" + this.bucket + "/" + this.file + "; did read " + ptr);
+
 	return buffer.buffer as ArrayBuffer;
+	
     }
     
 }
