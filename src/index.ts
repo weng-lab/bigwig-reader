4f12d88d4e2e4c0cdf14ad3ca56cb16cf0a411ed
hpratt <hpratt111@live.com>
2019-07-18 06:16:52 -0400

    export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { HeaderData, FileType, CommonHeader, ZoomLevelHeader, BWTotalSummary, ChromTree } from "./BigWigHeaderReader";
 export { BigWigData, BigBedData, BigZoomData, BigWigReader } from "./BigWigReader";
 export { DataLoader, AxiosDataLoader, FileDataLoader, ErrorType } from "./DataLoader";
+export { GoogleBucketDataLoader } from './GoogleDataLoader';
