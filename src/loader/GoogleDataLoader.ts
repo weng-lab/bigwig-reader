02f9a569c825f8f865efb4bc7e089eb284459d2f
hpratt <hpratt111@live.com>
2019-09-21 20:49:39 -0400

    fix issue where files smaller than the buffer size could not be read

--- src/loader/GoogleDataLoader.ts
+++ src/loader/GoogleDataLoader.ts
@@ -6,42 +6,43 @@ import { DataLoader } from './DataLoader';
  */
 export class GoogleBucketDataLoader implements DataLoader {
 
-	private storage_: Storage;
-	private bucket_: Bucket;
-	private file_: File;
+    private storage_: Storage;
+    private bucket_: Bucket;
+    private file_: File;
+    
+    constructor(private bucket: string, private file: string, private billedProject?: string) {
+	this.storage_ = new Storage();
+	this.bucket_ = this.storage_.bucket(bucket);
+	this.file_ = this.bucket_.file(file);
+    }
 
-	constructor(private bucket: string, private file: string, private billedProject?: string) {
-		this.storage_ = new Storage();
-		this.bucket_ = this.storage_.bucket(bucket);
-		this.file_ = this.bucket_.file(file);
+    async load(start: number, size?: number): Promise<ArrayBuffer> {
+
+	const fsize = +(await this.file_.getMetadata({
+	    userProject: this.billedProject
+	}))[0].size;
+	const bsize = (size && size <= fsize ? size : fsize) - start;
+	const stream = await this.file_.createReadStream({
+	    userProject: this.billedProject,
+	    start,
+	    end: start - 1 + (size || bsize)
+	});
+	if (!stream) throw new Error("could not open gs://" + this.bucket + "/" + this.file);
+	
+	const buffer: Uint8Array = new Uint8Array(bsize);
+	let ptr: number = 0;
+	for await (const chunk of stream) {
+	    if (chunk !== undefined && chunk !== null && chunk.length) {
+		buffer.set(chunk, ptr);
+		ptr += chunk.length;
+	    } else
+		throw new Error("could not read " + size + " bytes from gs://" + this.bucket + "/" + this.file + "; did read " + ptr);
 	}
-
-	async load(start: number, size?: number): Promise<ArrayBuffer> {
-
-		const bsize = size || +(await this.file_.getMetadata({
-			userProject: this.billedProject
-		}))[0].size - start;
-		const stream = await this.file_.createReadStream({
-			userProject: this.billedProject,
-			start,
-			end: start - 1 + (size || bsize)
-		});
-		if (!stream) throw new Error("could not open gs://" + this.bucket + "/" + this.file);
-
-		const buffer: Uint8Array = new Uint8Array(bsize);
-		let ptr: number = 0;
-		for await (const chunk of stream) {
-			if (chunk !== undefined && chunk !== null && chunk.length) {
-				buffer.set(chunk, ptr);
-				ptr += chunk.length;
-			} else
-				throw new Error("could not read " + size + " bytes from gs://" + this.bucket + "/" + this.file + "; did read " + ptr);
-		}
-		if (ptr !== buffer.length)
-			throw new Error("could not read " + size + " bytes from gs://" + this.bucket + "/" + this.file + "; did read " + ptr);
-
-		return buffer.buffer as ArrayBuffer;
-
-	}
-
+	if (ptr !== buffer.length)
+	    throw new Error("could not read " + size + " bytes from gs://" + this.bucket + "/" + this.file + "; did read " + ptr);
+	
+	return buffer.buffer as ArrayBuffer;
+	
+    }
+    
 }
