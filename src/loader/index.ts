4279dcf58fe852d9bf341e2850f200b7db31b578
Jason Brooks <jasonbrooks85@gmail.com>
2020-06-29 14:25:22 -0400

    Removed google specific code that has moved to bigwig-reader-gcp

--- src/loader/index.ts
+++ src/loader/index.ts
@@ -1,4 +1,3 @@
 export { DataLoader, ErrorType } from "./DataLoader";
 export { FileDataLoader } from "./FileDataLoader";
 export { AxiosDataLoader } from "./AxiosDataLoader";
-export { GoogleBucketDataLoader } from "./GoogleDataLoader";
