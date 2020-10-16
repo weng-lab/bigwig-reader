91357bc7eb7909ea630fdc87f0fc13318e75d9d6
Philip Adenekan <zoldello@gmail.com>
2020-10-16 06:27:05 -0700

    export ParseFunction

--- src/bigwig/index.ts
+++ src/bigwig/index.ts
@@ -3,4 +3,4 @@ export {
     BigBedDataNarrowPeak, BigBedDataBroadPeak, BigBedDataMethyl, BigBedDataTssPeak, BigBedDataIdrPeak,
     parseBigBedBroadPeak, parseBigBedIdrPeak, parseBigBedMethyl, parseBigBedNarrowPeak, parseBigBedTssPeak
 } from "./encodeBigBed";
-export { BigWigData, BigBedData, BigZoomData, BigWigReader, parseBigBed } from "./BigWigReader";
+export { BigWigData, BigBedData, BigZoomData, BigWigReader, parseBigBed, ParseFunction } from "./BigWigReader";
